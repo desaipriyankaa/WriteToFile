da6289b7e3811ca82a52085906640c574b54502c
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-05-18 13:40:23 +0530

    Add project files.

---
+++ Start/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"F:\PProject\Advanced\File IO\Start\Employee.txt";
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+
+            //Reading all lines of file
+            foreach(var line in lines)
+            {
+
+                Console.WriteLine(line);
+            }
+
+            //write data to file
+            Console.WriteLine("\n\nEnter data to write in file (name,surname,url): \n");
+            string content = Console.ReadLine();
+            lines.Add(content);
+            File.WriteAllLines(filepath, lines);
+            Console.WriteLine("\nFile updated....");
+            foreach (var line in lines)
+            {
+
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
